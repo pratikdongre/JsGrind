73f9e721a91cc80582a7900a14371207af6954e8
pratik <pratikdon2001@gmail.com>
2024-10-31 06:50:58 +0530

    2_object

---
+++ RepeatedObject/2_reference.js
@@ -0,0 +1,11 @@
+// the fundamental difference between object and other primitve data type is 
+// object is copied by reference 
+// where as other primitve data type are copied by value 
+
+let message = "hello";
+let phrase = message;
+// here we got copy of message 
+// so if we change message value to something other 
+// that wont change a shit about phrase now 
+
+// so we got two box 
