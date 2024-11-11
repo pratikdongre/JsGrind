5010a1aaeee1d8e396bd634d9d4e444b8d69e766
pratik <pratikdon2001@gmail.com>
2024-11-11 09:25:55 +0530

    8_objectToPrimitive_1

---
+++ RepeatedObject/8_ObjectToprimitve.js
@@ -0,0 +1,18 @@
+// what happens when objects are added obj1 + obj2 , subtracted obj1 - obj2 or printed using alert(obj)
+// in case of such operations objects are auto converted to primtives and then the operation is carreid out over theses primitives and results in primtive value 
+/*
+the result of an obj1 + obj2 cant be result into another object 
+
+object to primitve 
+there are two purposes 
+1. it will allow us to understand whats going on in case of coding mistakes when such operation happens accidentally
+2. there are exceptions where such operation are possible and look good eg subtraction or comparing dates
+    
+*/
+
+
+// conversion rules 
+/*
+
+
+*/
