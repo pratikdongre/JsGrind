8a533863869ef88feaf71a3a8e7fa98065a65d00
pratik dongre <pratikdon2001@gmail.com>
2024-12-18 21:43:13 +0530

    func.call

--- Functions/9_CallApplyDecorators.js
+++ Functions/9_CallApplyDecorators.js
@@ -43,12 +43,78 @@ we create a wrapper function that adds caching
 
     slow = cachingDecorator(slow);
     
-    console.log(slow(1));
-    console.log("Again :" + slow(1));
+    console.log(slow(1)); // slow(1) is cached and result is returned
+    console.log("Again :" + slow(1)); // slow(1) result returned from cache
     
-    console.log(slow(2));
-    console.log("Again :" + slow(2));
+    console.log(slow(2)); // slow(2) is cached and result is returned
+    console.log("Again :" + slow(2)); // slow(2) result returned from cache 
+
+    // cachingDecorator is a decorator a special function that takes another function
+    // and alter its behavior 
+
+    // the idea is that we can call CachingDecorator for any function 
+    /*
+    and it will return the caching wrapper 
+    we can have many funciton that could use such afeature 
+    all we need is to apply cachingDecorator to it 
+
+    by separating the caching from main code we also keep the main code simpler 
+
+    the result of cachingDecorator(func) is a wrapper function(x) that wraps the func(x)
+    into the logic
+
+    */
 
 
 }
 
+/*
+to summarize there are several benefits of using a separate cachingDecorator insteao of 
+altering the slow itself
+
+the cachingDecorator is reusable we can apply it to another function 
+the caching logic is separate it did not increase the complexity of slow itself 
+we can combine multiple decorators if needed 
+
+*/
+
+
+// using func.call for the context 
+// the function above decorationCaching is not suitable for object methods 
+
+
+{
+    let worker = {
+        someMethod(){
+            return 1;
+        },
+
+        slow(x){
+            console.log("Called with " , x );
+            return x * this.someMethod();
+        }
+
+    }
+    // function decoratorCaching
+
+    function cachingDecorator(func){
+        let cache = new Map();
+
+        return function(x){
+            if(cache.has(x)){
+                return cache.get(x);
+            }
+            let result = func(x);
+
+            cache.set(x,result);
+            return result;
+        };
+
+    }
+
+    console.log(worker.slow(1));
+
+    worker.slow = cachingDecorator(worker.slow);
+    // console.log(worker.slow(1));
+
+}
