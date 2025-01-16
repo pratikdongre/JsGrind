35ce58716e6454fb7e588f87b70a57ef8bfbf1bf
pratik <pratikdon2001@gmail.com>
2025-01-16 18:58:49 +0530

    1_modules.js

---
+++ Modules/2_importExport.js
@@ -0,0 +1,13 @@
+/*
+
+export and import 
+directives have sevaral syntax variants 
+
+*/
+
+// export before declaration 
+/*
+we can lable delcaration as exported by placing export before it 
+be it variable ,function or date 
+
+*/
