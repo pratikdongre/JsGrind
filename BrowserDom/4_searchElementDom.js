93c583aff302b66b62bb8dc40246b871c7fe9165
pratik <pratikdon2001@gmail.com>
2025-01-21 09:25:59 +0530

    searchElementDom

---
+++ BrowserDom/4_searchElementDom.js
@@ -0,0 +1,15 @@
+//Searching : getElements*, querySelector*
+/*
+dom navigation props are great when element are close to each other 
+what if they are not ? 
+How to get an arbitrary element of the page 
+
+*/
+
+// document.getElementById or just id 
+/*
+if an element has the id attribute we can get the element using the method 
+document.getElementById(id)
+no matter where it is 
+
+*/
