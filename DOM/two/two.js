dac6549e50ff0ffd39a76cb64b9db6e6ad858456
pratik dongre <pratikdon2001@gmail.com>
2025-02-16 20:05:28 +0530

    Friends with Madison Beer

--- DOM/two/two.js
+++ DOM/two/two.js
@@ -1,14 +1,34 @@
-let addFriend = document.querySelector('#request');
-let stats = document.querySelector('h5');
+let addFriend = document.querySelectorAll('#request');
+let stats = document.querySelectorAll('h5');
 let remove = document.querySelector('.remove');
 
-addFriend.addEventListener('click',function(){
-    stats.innerHTML = "Friends";
-    stats.color = "pink";
+console.log(addFriend[0]);
+
+
+addFriend[0].addEventListener('click',function(){
+    stats[0].innerHTML = "Friends";
+    stats[0].color = "pink";
 });
 
 remove.addEventListener('click',function(){
-    stats.textContent = "Unfriend";
-    stats.color = 'red'
+    stats[0].textContent = "Unfriend";
+    stats[0].color = 'red'
 })
 
+
+let check = 0;
+
+addFriend[1].addEventListener('click',function(){
+    if(check ==0){
+        stats[1].textContent = "Friends";
+        stats[1].color = 'pink'
+         check = 1;
+         addFriend[1].innerHTML = "Remove";
+    }
+    else {
+        stats[1].textContent = "UnFriend";
+        stats[1].color = 'red';
+        check = 0;
+        addFriend[1].innerHTML = "Request";
+    }
+})
