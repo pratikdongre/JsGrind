0dbc1f9444f382adb4f167c93b861e353c054b37
pratik <pratikdon2001@gmail.com>
2025-02-20 09:24:27 +0530

    last proj story

---
+++ BrowserDom/4_htmls/count.js
@@ -0,0 +1,16 @@
+// List of columns
+const columns = [
+    "appid", "billing", "billingdt", "body_content", "botid", "campaignid", "category", 
+    "category_id", "client_payload", "contactno", "countrycode", "createdt", "deletedt", 
+    "direction", "dlvrddt", "errcode", "errdesc", "expiration_timestamp", "faileddt", 
+    "fbtrace_id", "isdeducted", "isdeleted", "ishistoryprocessed", "ismodified", "messageid", 
+    "messagetype", "mobileno", "modifieddt", "msg_setting_id", "pricing_model", "profile_name", 
+    "rate", "readdt", "readstatus", "requestid", "retrycount", "retrydt", "sentdt", "sessid", 
+    "source", "status", "submissiontype", "subscriberid", "templateid", "userid", "uuid", "wabapayload"
+];
+
+// Count the number of columns
+const columnCount = columns.length;
+
+// Output the count
+console.log("Number of columns:", columnCount);
