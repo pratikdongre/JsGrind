0614ff08ff46efb127a45c6e3beef6b7e2ed1eac
pratik <pratikdon2001@gmail.com>
2025-02-28 20:16:58 +0530

    update on weather

--- DOM2/weatherapp/weather.js
+++ DOM2/weatherapp/weather.js
@@ -1,3 +1,7 @@
+https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&timeformat=unixtime
+
 export function getWeather(lat,lon,timezone){
-    
+
 }
+
+// this is it 
