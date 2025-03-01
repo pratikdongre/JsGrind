dd0c7db05f187dc3d4deb441de5bbfe9f616d8fc
pratik dongre <pratikdon2001@gmail.com>
2025-03-01 21:12:10 +0530

    weatherapp done

--- DOM2/weatherapp/weather.js
+++ DOM2/weatherapp/weather.js
@@ -1,7 +1,77 @@
-https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&timeformat=unixtime
+import axios from 'axios'
+
+// https://api.open-meteo.com/v1/forecast?1&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&precipitation_unit=inch&timeformat=unixtime
 
 export function getWeather(lat,lon,timezone){
-
+    return axios.get("https://api.open-meteo.com/v1/forecast?&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&precipitation_unit=inch&timeformat=unixtime", {
+    params :{
+        latitude : lat,
+        longitude : lon,
+        timezone,
+    },
+    })
+    
+    .then(({data}) => {
+        // return data ;
+        return {
+            current : parseCurrentWeather(data),
+            daily : parseDailyWeather(data),
+            hourly : parseHourlyWeather(data),
+        }
+    })
 }
 
-// this is it 
+
+function parseCurrentWeather({current,daily}){
+    const {temperature_2m : currentTemp,
+         wind_speed_10m : windSpeed,
+        weather_code : iconCode
+    } = current
+
+
+    const { temperature_2m_max : [maxTemp],
+            temperature_2m_min : [minTemp],
+            apparent_temperature_max : [maxFeelsLikeTemp],
+            apparent_temperature_min : [minFeelsLikeTemp],
+            precipitation_sum : [precip]
+
+
+    } = daily;
+
+    return {
+        currentTemp : Math.round(currentTemp),
+        highTemp : Math.round(maxTemp),
+        lowTemp : Math.round(minTemp),
+        highFeelslike : Math.round(maxFeelsLikeTemp),
+        lowFeelslike : Math.round(minFeelsLikeTemp),
+        windSpeed : Math.round(windSpeed),
+        precip : Math.round(precip * 100) / 100,
+        iconCode,
+    }
+}
+
+
+function parseDailyWeather({daily}){
+    return daily.time.map((time,index) => {
+        return {
+            timestamp : time*1000,
+            iconCode :  daily.weather_code[index],
+            maxTemp : Math.round(daily.temperature_2m_max[index])
+        }
+    })
+}
+
+
+function parseHourlyWeather({hourly,current}){
+    return hourly.time.map((time,index) => {
+        return {
+            timestamp : time *1000,
+            iconCode : hourly.weather_code[index],
+            temp : Math.round(hourly.temperature_2m[index]),
+            feelsLike : Math.round(hourly.apparent_temperature[index]),
+            windSpeed : Math.round(hourly.wind_speed_10m[index]),
+            precip : Math.round(hourly.precipitation[index] *100 )/100
+        }
+    }).filter(({timestamp}) => timestamp >=current.time * 1000);
+
+}
