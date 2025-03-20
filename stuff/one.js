ac26f87eaffa9f1427e66634cc2766df79a1c37c
pratik <pratikdon2001@gmail.com>
2025-03-20 06:59:01 +0530

    upd

--- stuff/one.js
+++ stuff/one.js
@@ -1,9 +1,10 @@
-let arr = [80982, 84701, 82519, 85817];
+let arr = [84426, 81729, 79766, 83483, 80305, 82328, 81988, 86214];
 arr.map((x) => {
   let sixty = Math.floor((60 / 100) * x);
 
   let fourty = x - sixty;
   console.log(
-    `for ${x} the 60% is ${Math.floor(sixty)} and 40% is ${Math.floor(fourty)}`
+    // `for ${x} the 60% is ${Math.floor(sixty)} and 40% is ${Math.floor(fourty)}`
+    `${Math.floor(fourty)}`
   );
 });
